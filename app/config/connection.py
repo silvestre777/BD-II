d04b15d75894d6266654e91b861c9e55586f27cd
Silvestre Ferreira <168732596+silvestre777@users.noreply.github.com>
2024-10-21 12:40:04 +0000

    Atualização usuario

--- app/config/connection.py
+++ app/config/connection.py
@@ -17,7 +17,7 @@ db_name = "meu_banco"
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 
-db = create_engine(DATA_URL)
+db = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=db)
 session = Session()
 
---
+++ app/models/usuario.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+from config.connection import db
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__="usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(150))
+    email = Column(String(150), unique = True)#unique mostra que o email deve ser unico, nao pode ser repetido
+    senha = Column(String(150))
+
+    def __init__(self, nome:str, email:str, senha:str):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+#Criando tabela no banco de dados.
+Base.metadata.create_all(brind=db)
