2bdb703f1062cdbc2024f73cb5cb37c439823dfd
Silvestre Ferreira <168732596+silvestre777@users.noreply.github.com>
2024-10-21 12:32:03 +0000

    Configurando conexão com o banco de dados

--- app/config/connection.py
+++ app/config/connection.py
@@ -2,6 +2,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
-#URL DE CONEXAO PARA BD MySQL
-DATABASE_URL = f"mysql+pymysql://usuario:senha@host:porta/nome_bd"
+#Parametros de conexao com MySQL
 
+db_user = "user"
+db_password = "user_password"
+#localhost = minha maquina
+db_host = "localhost"
+db_port = "3306"
+db_name = "meu_banco"
+
+#URL DE CONEXAO PARA BD MySQL
+#DATABASE_URL = f"mysql+pymysql://usuario:senha@host:porta/nome_bd"
+
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+
+db = create_engine(DATA_URL)
+Session = sessionmaker(bind=db)
+session = Session()
+
+@contextmanager
+def git_db():
+    db = Session() # Cria uma sessão para ações no banco de dados.
+    try:
+        yield db # Caso a sessão realize todas as tarefas, salva a operação.
+        db.commit()
+    except Exception as erro:
+        db.rollback() # Desfaz todas alterações em caso de erro em alguma operação.
+        raise erro # Lança uma exceção.
+    finally:
+        db.close() #Fecha a sessão com o bando de dados.
